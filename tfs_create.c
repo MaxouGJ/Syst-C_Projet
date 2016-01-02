c7ed3879c796a02bb6b0b4857603207206cd0d7b
Hafca <hafca.tirichine@gmail.com>
2016-01-02 18:26:38 +0100

    ajout d'une close a tfs_create

--- tfs_create.c
+++ tfs_create.c
@@ -1,7 +1,5 @@
 #include "tfs.h"
 
-
-
 //-------------------------------------------------
 //Fonction permetant de recupérer une taille valide
 int giveSize(char *taille){
@@ -29,44 +27,43 @@ char * giveName(char *name){
 //Main permetant de créer le disque
 int main(int argc,char *argv[]){
 	int diskSize = 0;
-	char *diskName = "disk";
+	char *diskName = "disk.tfs";
 	FILE *fichier;
-
-	if (strcmp(argv[1], "-s") != 0){	//Si l'option n'est pas "-s" on affiche une erreur
-		fprintf(stderr, "ERREUR DE SYNTAXE: Il faut specifier le nombre de blocs constituant le disque (option -s) \n");
+	
+	//Si l'utilisateur tape tfs_create sans arguments
+	if(argc==1){
+		fprintf(stderr, "ERREUR DE SYNTAXE: Il manque des arguments\n tfs_create -s size [name]\n");
 	}
-    
-    else {
-        
-		if(argc >= 3)
-        {
+	else{
+	//Si l'option n'est pas "-s" on affiche une erreur
+	if (strcmp(argv[1], "-s") != 0){	
+		 fprintf(stderr, "ERREUR DE SYNTAXE: Il faut specifier le nombre de blocs constituant le disque (option -s) \n");
+	}
+ 
+    	else {
+        	if(argc >= 3){
 			if (argc > 3){	//L'utilisateur a choisi un nom au disque
 				diskName = strcat(argv[3],".tfs");
 			}
-            
-            else {
-				diskName = "disk.tfs";
-			}
-            
 			diskSize = giveSize(argv[2]);
 			diskName = giveName(diskName);
 			fichier = fopen(diskName, "w+");
-			truncate(diskName, TAILLE_BLOC * diskSize);		//Permet de donner une taille x * 1024 au fichier
+			truncate(diskName, TAILLE_BLOC * diskSize);	//Permet de donner une taille x * 1024 au fichier
 
 
             //------------------------------------------------------------------------------------------------------
             //On a un fichier diskName.tfs crée avec une taille de 1024*taille soit taille bloc de 1024 octet chacun
 			
-			fputc(htole32(diskSize), fichier);  //taille du disque
-			fputc(htole32(1), fichier);			// 1 seul partition au départ
+			fputc(htole32(diskSize), fichier);  	//taille du disque
+			fputc(htole32(1), fichier);		// 1 seul partition au départ
 			fputc(htole32(diskSize),fichier);
 			printf("Le disque %s a été créé avec succès \n", diskName);
 			
 		}
         
-        else {	//Il manque des argument dans l'appel a la commande
-			fprintf(stderr, "ERREUR DE SYNTAXE: il n'y a pas assez d'argument");
+        	else {	//Il manque des argument dans l'appel a la commande
+			fprintf(stderr, "ERREUR DE SYNTAXE: Il manque des arguments\n tfs_create -s size [name]\n");
 		}
-        
+        }
 	}
 }
